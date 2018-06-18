638b3552bed6c60fac6889d50ba7b37871b1cb5f
joshMEA <jmfmealing91@gmail.com>
2018-06-18 16:27:32 +1000

    added save function, need to work on error handlers in next commit

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,14 @@
 
 const tiles = document.querySelectorAll('.tile');
 const colorSave = document.querySelectorAll('.saved-color');
-const code = document.querySelector('.code');
-const allColors = document.querySelectorAll('.saved-color, .tile');
+const colorCode = document.querySelector('.color-code');
+const savedColorCode = document.querySelector('.saved-color-code');
+const allTiles = document.querySelectorAll('.tile');
+const savedColor = document.querySelectorAll('.saved-color');
 const colorGenBtn = document.querySelector('.gen-color-btn');
-const saveBtn = document.querySelector('.save-palette-btn');
+const saveBtn = document.querySelector('.save-color-btn');
+let clicked;
+let savedClicked;
 
 // Event Listeners
 
@@ -16,14 +20,37 @@ colorGenBtn.addEventListener('click', () => {
     tiles.forEach(tile => tile.style.backgroundColor = genColor());
 });
 
-//displays rgba code in (p.code)
-allColors.forEach(tile => tile.addEventListener('click', () => {
-    code.textContent = tile.style.backgroundColor;
+// Applies color to the palette
+saveBtn.addEventListener('click', () => {
+    // Error handler
+    if(clicked === undefined && savedClicked === undefined) { // if neither is selected
+        console.log('select a colour and a palette to save it to');
+    } else if (savedClicked === undefined) { // if no palette tab is selected
+        console.log('Please select a palette to save this color to');
+    } else if (clicked === undefined) { // if no color is selected
+        console.log('please select a color to save to this palette');
+    } else {
+        savedClicked.style.backgroundColor = clicked.style.backgroundColor;
+        console.log('color saved!');
+    }
+
+});
+
+// handles a click on a generated color tile
+allTiles.forEach(tile => tile.addEventListener('click', () => {
+    colorCode.textContent = tile.style.backgroundColor;
+    clicked != undefined ? clicked.classList.remove('clicked'): false;
+    tile.classList.add('clicked');
+    clicked = document.querySelector('.clicked');
 }));
 
-
-
-
+// handles a click on a saved color tile
+savedColor.forEach(tile => tile.addEventListener('click', () => {
+    savedColorCode.textContent = tile.style.backgroundColor;
+    savedClicked != undefined ? savedClicked.classList.remove('saved-clicked'): false;
+    tile.classList.add('saved-clicked');
+    savedClicked = document.querySelector('.saved-clicked');
+}));
 
 
 
@@ -42,4 +69,3 @@ function genColor() {
     colorCode += ')';
     return colorCode;
 }
-
