52776eb22797a7c33b40397f48e69b4dbf7ac93c
joshMEA <jmfmealing91@gmail.com>
2018-06-18 14:17:26 +1000

    Initial commit of files. JS needs to be touched up to make sure the opacity of all colours is not too low(<0.4)

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+
+
+// selectors
+
+const tiles = document.querySelectorAll('.tile');
+const colorSave = document.querySelectorAll('.saved-color');
+const code = document.querySelector('.code');
+const allColors = document.querySelectorAll('.saved-color, .tile');
+const colorGenBtn = document.querySelector('.gen-color-btn');
+const saveBtn = document.querySelector('.save-palette-btn');
+
+// Event Listeners
+
+//applys random color to all tiles
+colorGenBtn.addEventListener('click', () => {
+    tiles.forEach(tile => tile.style.backgroundColor = genColor());
+});
+
+//displays rgba code in (p.code)
+allColors.forEach(tile => tile.addEventListener('click', () => {
+    code.textContent = tile.style.backgroundColor;
+}));
+
+
+
+
+
+
+
+// functions
+
+//color generate function
+
+function genColor() {
+    let colorCode = 'rgba(';
+    for(let i = 0; i < 3; i++) {    // generates RGB code
+        let num = Math.floor(Math.random() * 255) + 1;
+        colorCode += num;
+        colorCode += ',';        
+    }
+
+    let aVal = Math.random().toFixed(2);
+
+    colorCode += aVal; 
+    colorCode += ')';
+    return colorCode;
+}
+
